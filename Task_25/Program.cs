94802eed6ba6f40f81cc218c757fb6e48edea10c
Karatino <viewkaratsonic@mail.ru>
2022-09-10 10:48:56 +0500

    Added exponentiation

---
+++ Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+Решить задачу с использованием методов.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Double GetDegree(double a, Double b)
+{
+
+    double degree = Math.Pow(a, b);
+
+    Console.WriteLine($"Готово! {a} в степени {b} равно {degree}!");
+
+    return degree ;
+}
+
+Console.Write("Пожалуйста, введите число, которое вы хотите возвести в степень: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write($"Спасибо, теперь введите степень, в которую хотите возвести число {a}: ");
+int b = int.Parse(Console.ReadLine());
+
+Double DegreeNumber = GetDegree(a, b);
+Console.WriteLine(DegreeNumber);
+
