f84d664c297e0ecd87e8c274d1093c8ddc58ad73
Karatino <viewkaratsonic@mail.ru>
2022-09-13 09:15:09 +0500

    Array output

---
+++ Task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+Элементы массива вводятся пользователем.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+
+Console.Write("Введите количество элементов масива: ");
+
+int elementsCount = int.Parse(Console.ReadLine());
+
+int[] myArray = new int[elementsCount];
+
+for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Введите элемент массива по индексом {i}: ");
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+
+
+
+System.Console.WriteLine("Вывод масива: ");
+
+for (int i = 0; i < myArray.Length; i++)
+    {
+            Console.WriteLine(myArray[i]);
+    }
+
+Console.ReadLine();
