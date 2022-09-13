a53e09297c762a28e78446a8bdf4995017f1c76e
Karatino <viewkaratsonic@mail.ru>
2022-09-13 08:37:08 +0500

    Added calculation of the sum of digits

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+static int SumNumbers(int value)
+    {
+        int result = 0;
+        while (value > 0)
+        {
+            result += value % 10;
+            value = value / 10;
+        }
+        return result;
+        Console.WriteLine(result);
+    }
+
+Console.Write("Введите любое число: ");
+int num = int.Parse(Console.ReadLine());
+int result = SumNumbers(num);
+Console.WriteLine(result);
